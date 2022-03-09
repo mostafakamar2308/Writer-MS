3eeb6a30e7ed8683f7347af4deb3e1daccd89dbb
Mostafa Kamar <moustafa.31011197@med.tanta.edu.eg>
2022-03-09 04:43:25 +0200

    finished the header and started in the intro Section

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "/src/index.js",
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
